70e1ec5ac85e6e31efc8ec203940de0f6179be8d
wenxing.luo <wenxing.luo@ximalaya.com>
2022-01-08 10:49:23 +0800

    配置修改

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -1,8 +1,8 @@
-drop database if exists litemall;
-drop user if exists 'litemall'@'%';
+drop database if exists mall;
+drop user if exists 'dev'@'%';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
-create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
-use litemall;
-create user 'litemall'@'%' identified by 'litemall123456';
-grant all privileges on litemall.* to 'litemall'@'%';
+create database mall default character set utf8mb4 collate utf8mb4_unicode_ci;
+use mall;
+create user 'dev'@'%' identified by 'Season!0316';
+grant all privileges on mall.* to 'dev'@'%';
 flush privileges;
